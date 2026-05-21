dd832ed0265596b0dc32c6446f889b6cbcd3dab6
matkokrnic <matko.krnic@fer.hr>
2026-05-21 20:06:21 +0200

    C: IncarnationPickup ažuriran

--- scripts/pickups/IncarnationPickup.gd
+++ scripts/pickups/IncarnationPickup.gd
@@ -5,5 +5,5 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
-		# pitaj osobu A kako se zove funkcija za dodavanje inkarnacije na GameManageru
+		GameManager.add_incarnation()
 		queue_free()
