5f23094f9b5993a159c22f354c3d90cfa78c11b5
matkokrnic <matko.krnic@fer.hr>
2026-05-21 19:43:31 +0200

    C: SecondsExtra skripta gotova

--- scripts/pickups/SecondsExtra.gd
+++ scripts/pickups/SecondsExtra.gd
@@ -5,5 +5,5 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
-		# pitaj osobu A kako se zove funkcija za dodavanje sekundi na timeru
+		GameManager.add_seconds(5.0)
 		queue_free()
